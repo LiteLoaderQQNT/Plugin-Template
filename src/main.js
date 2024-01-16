ac9f86bed25e0853435654f94b489381134b95d8
沫烬染 <mojinran@outlook.com>
2024-01-16 19:29:45 +0800

    LiteLoaderQQNT 1.0.0

--- src/main.js
+++ src/main.js
@@ -1,20 +1,8 @@
 // 运行在 Electron 主进程 下的插件入口
 
 
-// 加载插件时触发
-function onLoad(plugin) {
-
-}
-
 
 // 创建窗口时触发
-function onBrowserWindowCreated(window, plugin) {
-
-}
-
-
-// 这两个函数都是可选的
-module.exports = {
-    onLoad,
-    onBrowserWindowCreated
+module.exports.onBrowserWindowCreated = window => {
+    // window 为 Electron 的 BrowserWindow 实例
 }
--- src/renderer.js
+++ src/renderer.js
@@ -1,20 +1,7 @@
 // 运行在 Electron 渲染进程 下的页面脚本
 
 
-// 页面加载完成时触发
-function onLoad() {
-
-}
-
-
 // 打开设置界面时触发
-function onConfigView(view) {
-
-}
-
-
-// 这两个函数都是可选的
-export {
-    onLoad,
-    onConfigView
+export const onSettingWindowCreated = view => {
+    // view 为 Element 对象，修改将同步到插件设置界面
 }
